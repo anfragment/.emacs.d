eeba93946eab40368006b5bc786d6e16893de77b
grumpydog <grumpydog@tutanota.com>
2022-05-29 15:01:57 +0600

    idk

--- init.el
+++ init.el
@@ -3,15 +3,34 @@
       '(("gnu" . "http://elpa.gnu.org/packages/")
 	("melpa" . "http://melpa.org/packages/")))
 
-
-(setq apropos-sort-by-scores t)
-(global-set-key (kbd "M-o") 'other-window)
-(windmove-default-keybindings)
-(fset 'yes-or-no-p 'y-or-n-p)
-(setq create-lockfiles nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("fc48cc3bb3c90f7761adf65858921ba3aedba1b223755b5924398c666e78af8b" default))
+ '(package-selected-packages
+   '(nyan-mode darcula-theme ample-theme multiple-cursors pdf-view-restore pdf-tools ivy rainbow-delimiters neotree projectile resize-window ace-window vterm eyebrowse move-text helm typescript-mode))
+ '(typescript-indent-level 2)
+ '(warning-suppress-types '((comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 (set-frame-font "SF Mono 10")
+(fset 'yes-or-no-p 'y-or-n-p)
+(setq create-lockfiles nil)
+(setq ring-bell-function 'ignore)
+(setq inhibit-startup-screen t)
 
+(windmove-default-keybindings)
+(global-set-key (kbd "M-o") 'other-window)
+
+(fringe-mode 0)
 (tool-bar-mode 0)
 (tab-bar-mode 0)
 (tab-line-mode 0)
@@ -19,22 +38,23 @@
 (scroll-bar-mode 0)
 (winner-mode 1)
 (eyebrowse-mode 1)
-(rainbow-delimiters-mode 1)
 (electric-pair-mode 1)
-(rainbow-delimiters-mode 1)
 (ivy-mode 1)
 (recentf-mode 1)
-(rainbow-delimiters-mode 1)
+
+;; global-rainbow-delimiters-mode
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 
 ;; show paren mode
 (show-paren-mode)
 
 ;; projectile mode
 (projectile-mode 1)
-(setq projectile-project-search-path '("~/dev/" "~/dev/ps/"))
+(setq projectile-project-search-path '("~/dev/"))
 
 ;; theme
-(load-theme 'zenburn t)
+(load-theme 'darcula t)
+(enable-theme 'darcula)
 
 ;; mulitple-cursors
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
@@ -46,6 +66,11 @@
 (global-set-key (kbd "M-o") 'ace-window)
 (setq aw-dispatch-always 1)
 
+;; nyan-mode
+(when (display-graphic-p)
+  (setq nyan-animate-nyancat t)
+  (nyan-mode 1))
+ 
 ;; pdf-tools
 (pdf-tools-install)
 (define-globalized-minor-mode my-global-linum-mode linum-mode
@@ -57,7 +82,12 @@
 (add-hook 'pdf-view-mode-hook 'pdf-view-restore-mode)
 
 ;; temporary files
-(setq backup-directory-alist '((".+" . "~/.emacs.d/.backup-files")))
+(setq backup-directory-alist `(("." . "~/.emacs-saves")))
+(setq backup-by-copying t)
+
+;; display help in current buffer
+(add-to-list 'display-buffer-alist
+             '("*Help*" display-buffer-same-window))
 
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "M-x") 'helm-M-x)
@@ -70,11 +100,24 @@
 (global-set-key (kbd "M-p") 'scroll-down-line)
 (move-text-default-bindings)
 (setq-default frame-title-format '("%f"))
+
 (defun show-file-name ()
   "Show the full path file name in the minibuffer."
   (interactive)
   (message (buffer-file-name)))
-(setq inhibit-startup-screen t)
+
+(defun ide-layout ()
+  (interactive)
+  (setq starting-window (selected-window))
+  (neotree-projectile-action)
+  (select-window starting-window)
+  (setq starting-window-new-height
+	(floor (* (window-total-height) 0.8)))
+  (select-window (split-window-below starting-window-new-height))
+  (projectile-run-vterm)
+  (select-window starting-window))
+
+(global-set-key [f6] 'ide-layout)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -84,11 +127,6 @@
  '(custom-safe-themes
    '("fc48cc3bb3c90f7761adf65858921ba3aedba1b223755b5924398c666e78af8b" default))
  '(package-selected-packages
-   '(pdf-view-restore pdf-tools ivy rainbow-delimiters zenburn-theme neotree projectile resize-window ace-window vterm eyebrowse move-text helm typescript-mode))
- '(typescript-indent-level 2))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+   '(nyan-mode darcula-theme ample-theme multiple-cursors pdf-view-restore ivy rainbow-delimiters neotree projectile resize-window ace-window vterm eyebrowse move-text helm typescript-mode))
+ '(typescript-indent-level 2)
+ '(warning-suppress-types '((comp))))
