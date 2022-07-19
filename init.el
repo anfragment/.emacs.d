ca8b36bfe35c12d7f0722f7e83a797ded2c0871e
grumpydog <grumpydog@tutanota.com>
2022-07-19 21:27:54 +0600

    feat: move-text, dired-open-file

--- init.el
+++ init.el
@@ -208,3 +208,16 @@
   (setq nasm-basic-offset 4)
   (electric-indent-local-mode -1))
 
+(use-package move-text
+  :config
+  (move-text-default-bindings))
+
+
+(defun dired-open-file ()
+  "In dired, open the file named on this line."
+  (interactive)
+  (let* ((file (dired-get-filename nil t)))
+    (call-process "xdg-open" nil 0 nil file)))
+
+(define-key dired-mode-map (kbd "C-c o") 'dired-open-file)
+
